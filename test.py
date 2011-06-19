d16a7db5ae06af2b2e8ad5850efc0e61b340dc03
Anthony Van de Gejuchte <anthonyvdg@yahoo.com>
2011-06-19 15:49:26 +0200

    Test the output with the expected solutions

--- test.py
+++ test.py
@@ -49,7 +49,7 @@ def main():
     [5, 0, 0, 6, 4, 0, 7, 0, 0],
   ]
 
-  solutions = [
+  expected = [
     [8, 9, 3, 1, 5, 4, 6, 2, 7],
     [5, 2, 1, 9, 6, 7, 4, 8, 3],
     [7, 6, 4, 3, 8, 2, 9, 5, 1],
@@ -59,8 +59,7 @@ def main():
     [9, 3, 2, 6, 7, 8, 5, 1, 4],
     [6, 1, 7, 5, 4, 3, 8, 9, 2],
     [4, 8, 5, 2, 1, 9, 7, 3, 6],
-  ],
-  [
+  ], [
     [7, 8, 2, 3, 1, 5, 4, 9, 6],
     [5, 3, 9, 6, 7, 4, 8, 1, 2],
     [1, 6, 4, 8, 2, 9, 3, 7, 5],
@@ -70,8 +69,7 @@ def main():
     [9, 4, 1, 2, 5, 3, 7, 6, 8],
     [6, 5, 8, 1, 9, 7, 2, 4, 3],
     [3, 2, 7, 4, 8, 6, 1, 5, 9],
-  ],
-  [
+  ], [
     [6, 4, 2, 8, 5, 7, 9, 3, 1],
     [3, 5, 8, 9, 2, 1, 4, 6, 7],
     [7, 9, 1, 4, 3, 6, 8, 5, 2],
@@ -81,8 +79,7 @@ def main():
     [8, 3, 7, 1, 9, 2, 5, 4, 6],
     [5, 6, 4, 7, 8, 3, 2, 1, 9],
     [1, 2, 9, 6, 4, 5, 7, 8, 3],
-  ],
-  [
+  ], [
     [9, 5, 7, 4, 1, 3, 8, 2, 6],
     [4, 2, 8, 5, 9, 6, 1, 7, 3],
     [6, 3, 1, 8, 2, 7, 5, 4, 9],
@@ -94,8 +91,9 @@ def main():
     [5, 8, 9, 6, 4, 2, 7, 3, 1],
   ]
 
-  for sudoku in sudokus:
-    solveSudoku(sudoku)
+  for x, sudoku in enumerate(sudokus):
+    solve = solveSudoku(sudoku)
+    compareSudoku(expected[x], solve)
 
 """
   Solves a sudoku while displaying some basic debug data
@@ -114,6 +112,8 @@ def solveSudoku(sudoku):
   print '--------------------------------------------------------------------------------'
   print
 
+  return grid.getSudoku()
+
 """
   Printing a 2d sudoku
 """
@@ -122,6 +122,17 @@ def outputSudoku(sudoku):
     for columns in rows:
        sys.stdout.write(str(columns))
     print
+
+"""
+  Compares a sudoku against a given solution
+"""
+def compareSudoku(expected, result):
+  for x, row in enumerate(expected):
+    for y, cell in enumerate(row):
+      if result[x][y] == 0:
+        continue # not found
+      if result[x][y] != cell:
+        print str([x, y]) + " expected to be " + str(cell) + " but found " + str(result[x][y])
 
 if __name__ == '__main__':
   main()
